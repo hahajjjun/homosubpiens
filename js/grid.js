672d31994062d9692efe605b92f1bd3fec54cf12
haha <haha@haha.kr>
2021-02-24 22:40:36 +0900

    day 2

---
+++ js/grid.js
@@ -0,0 +1,28 @@
+function deleteGrid() {
+    $(".grid").remove();
+}
+
+function createGrid() {
+    var size =window.innerWidth/20;
+    //console.log(size);
+    deleteGrid();
+    var ratioW = Math.floor($(window).width()/size),
+        ratioH = Math.floor($(window).height()/size);
+
+    var parent = $('<div />', {
+        class: 'grid',
+        width: ratioW  * size,
+        height: ratioH  * size
+    }).addClass('grid').appendTo('body');
+
+    for (var i = 0; i < ratioH; i++) {
+        for(var p = 0; p < ratioW; p++){
+            $('<div />', {
+                width: size - 1,
+                height: size - 1
+            }).appendTo(parent);
+        }
+    }
+    
+}
+createGrid();
